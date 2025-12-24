493c010f7d96ca0e2d299936a13f9a6ebccfd921
Hari Shankar <harishankarn04@gmail.com>
2025-12-24 17:17:13 +0530

    update lv_hb_canvas_label header file

--- source/lv_hb_canvas_label.h
+++ source/lv_hb_canvas_label.h
@@ -10,10 +10,4 @@ lv_obj_t * lv_hb_label_create(lv_obj_t * parent,
                               int x,
                               int y);
 
-lv_obj_t * lv_hb_label_create_marquee(lv_obj_t * parent,
-                              const char * text,
-                              const char * font_path,
-                              int font_px,
-                              int x,
-                              int y);
 #endif
